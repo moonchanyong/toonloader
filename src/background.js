75c8095d42b7dbf234d20d8984ca5df546acfa86
mcy <201102430@cs-cnu.org>
2018-05-23 01:30:29 +0900

    update background.js: daum parser

--- src/background.js
+++ src/background.js
@@ -1,25 +1,44 @@
-const naverSerarchURI= 'http://comic.naver.com/search.nhn?keyword=';
+const naverSerarchURI = 'http://comic.naver.com/search.nhn?keyword=';
+const daumSearchURI = 'http://webtoon.daum.net/data/pc/search/suggest?q='
 const parser=new DOMParser();
-const parseList = {
-  'naver':parseNaverList
-}
-let platform = 'naver';
+const parseList = [
+  { parser: parseNaverList, uri :naverSerarchURI },
+  { parser: parseDaumList, uri: daumSearchURI },
+]
+
+let test;
+// search button listener
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('button').addEventListener('click', search);
 });
 
-let test = '';
-
 // 구조적문법 지려버렷고
 function search() {
-  let value = document.getElementById('query').value;
-  let uri = `${naverSerarchURI}${encodeURI(value)}`;
+  let keyword = encodeURI(document.getElementById('query').value);
 
-  httpGet(uri).then(({currentTarget})=>{
-    let response = currentTarget.response;
-    parseList[platform](response).then((list) => {
-      console.log(list);
-    })
+  parseList.forEach(({parser, uri}) => {
+    httpGet(`${uri}${keyword}`).then(({currentTarget})=> {
+      let response = currentTarget.response;
+      parser(response).then((list) => {
+        list.forEach((item) => {
+          console.log(item);
+        });
+      });
+    });
+  })
+}
+
+function parseDaumList(dataString) {
+  let parsingData = JSON.parse(dataString).data;
+  return new Promise((resolve, reject) => {
+    let ret = [];
+    test = parsingData;
+    ret.push(...parsingData.map(({title, nickname}) => ({
+      id: nickname,
+      title: title,
+      platform: 'daum',
+    })));
+    resolve(ret);
   })
 }
 
@@ -35,18 +54,16 @@ function parseNaverList(htmlText) {
         Array.from(children).forEach(({children}) => {
           Array.from(children[0].getElementsByTagName('a'))
             .forEach(({search, innerText}) => {
-              ret.push({search, innerText});
+              ret.push({
+                id: search,
+                title: innerText,
+                platform: 'naver',
+              });
             });
         });
       });
     resolve(ret);
   });
-}
-
-function opennewTab() {
-  return new Promise((resolve, reject) => {
-    resolve()
-  })
 }
 
 function httpGet(uri) {
